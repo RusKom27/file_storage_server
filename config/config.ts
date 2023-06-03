0521e646431a892cb06f1a0e0afda5f2d75a4b5a
RusKom27 <48696676+RusKom27@users.noreply.github.com>
2023-06-03 21:46:59 +0300

    update gitignore

---
+++ config/config.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+
+const MONGO_URL = (process.env.DEVELOPE ? process.env.LOCAL_MONGO_URL : process.env.MONGO_URL) || "";
+const SERVER_PORT = process.env.SERVER_PORT || '3000';
+// const CLIENT_URL = process.env.DEVELOPE ? process.env.LOCAL_CLIENT_URL : process.env.CLIENT_URL;
+
+const config = {
+    port: SERVER_PORT,
+    // client: CLIENT_URL,
+    mongo_url: MONGO_URL,
+    corsOptions: {
+        credentials: true,
+        // origin: CLIENT_URL,
+        optionSuccessStatus: 200,
+    },
+};
+
+export default config;
+
+
+
